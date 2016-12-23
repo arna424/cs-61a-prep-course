04c5b1e6e288b64a971b194368a69b606eeca17d
arna424 <ar.na.424@berkeley.edu>
2016-12-22 21:06:57 -0800

    Ch2

---
+++ Chapter5/chapter5.rb
@@ -0,0 +1,12 @@
+puts 'Hello there what is your first name?'
+first=gets.chomp
+puts 'Hello there what is your middle name?'
+middle=gets.chomp
+puts 'Hello there what is your last name?'
+last=gets.chomp
+puts 'Your name is ' +first+' '+middle+' '+last+'.'
+
+puts'what is your favorite number?'
+number=gets.chomp
+number=number+1
+puts'but ' + number + ' is so much better. It\'s bigger and better'
