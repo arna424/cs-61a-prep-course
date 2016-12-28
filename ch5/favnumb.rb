e3912cbffd6b8d901ae01d1a3effb6b70ca7e7dc
arna424 <ar.na.424@berkeley.edu>
2016-12-27 18:59:03 -0800

    separated

---
+++ ch5/favnumb.rb
@@ -0,0 +1,4 @@
+puts'what is your favorite number?'
+number=gets.chomp
+number=number+1
+puts'but ' + number + ' is so much better. It\'s bigger and better'
---
+++ ch5/fullnamegreeting.rb
@@ -0,0 +1,7 @@
+puts 'Hello there what is your first name?'
+first=gets.chomp
+puts 'Hello there what is your middle name?'
+middle=gets.chomp
+puts 'Hello there what is your last name?'
+last=gets.chomp
+puts 'Your name is ' +first+' '+middle+' '+last+'.'
