dbe2cafbcf34c7c5fd3b4eb906f5d5f9e8f06fb9
arna424 <ar.na.424@berkeley.edu>
2016-12-28 12:14:30 -0800

    added ch6 code

---
+++ ch6/tableofcontenets.rb
@@ -0,0 +1,6 @@
+linew=40
+puts'                   Table of Contents'.center(linew)
+puts''
+puts('Chapter 1: Getting Started'.ljust(linew) + 'page 1'.rjust(linew))
+puts('Chapter 2: Numbers'.ljust(linew) + 'page 19'.rjust(linew))
+puts('Chapter 3: Letters'.ljust(linew) + 'page 13'.rjust(linew))
